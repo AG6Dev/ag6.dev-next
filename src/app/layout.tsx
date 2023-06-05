9c71d7c50bcc2ddf40da9438bc7596f237f666e6
AG6 <60290746+AG6Dev@users.noreply.github.com>
2023-06-06 00:19:43 +0100

    start work on the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 
     return (
         <html lang="en">
+        <Navbar />
         {children}
         </html>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,34 @@
-export default function Navbar() {
-    return <>
+import Image from "next/image";
+import Link from "next/link";
 
-    </>
+import icon from '../assets/icon.png'
+
+export default function Navbar() {
+    require('../styles/navbar.css')
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-left-links">
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                </ul>
+            </div>
+
+            <div className="navbar-middle">
+                <div className="navbar-brand">
+                    <Image src={icon} alt="Icon" className="navbar-icon"/>
+                    <h1>AG6Dev</h1>
+                </div>
+                <span className="navbar-middle-bar"></span>
+            </div>
+
+            <div className="navbar-right-links">
+                <ul>
+                    <li><Link href="/projects">Projects</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
+                </ul>
+            </div>
+        </nav>
+)
 }

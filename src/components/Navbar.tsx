34bd842fd0005b8da022f80ddfd46acf38d1cf49
AG6 <60290746+AG6Dev@users.noreply.github.com>
2023-06-10 00:31:04 +0100

    final navbar cleanup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,20 +7,19 @@ export default function Navbar() {
     require("../styles/navbar.css")
 
     return (
-        <nav className="navbar">
-            <div className="navbar-left-links">
+        <nav id="navbar-wrapper">
+            <div id="navbar-left-links">
                 <Link href="/" className="navbar-link">Home</Link>
                 <Link href="/about" className="navbar-link">About</Link>
             </div>
 
-            <div className="navbar-middle">
-                <div className="navbar-brand">
-                    <Image src={icon} alt="Icon" className="navbar-icon"/>
+            <div id="navbar-middle">
+                <div id="navbar-brand">
+                    <Image src={icon} alt="AG6/AG6Dev" id="navbar-icon"/>
                 </div>
-                <span className="navbar-middle-bar"></span>
             </div>
 
-            <div className="navbar-right-links">
+            <div id="navbar-right-links">
                 <Link href="/projects" className="navbar-link">Projects</Link>
                 <Link href="/contact" className="navbar-link">Contact</Link>
             </div>
